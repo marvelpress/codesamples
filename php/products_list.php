0dbe59d5119fd8a268fcaedf60369121e303c2fb
Lewis Pollard <lewispollard@me.com>
2012-09-20 10:48:19 +0100

    Added products list sample

---
+++ php/products_list.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Example of retrieving a product list from the Marvelpress Order API
+ *
+ * @author Oliver Smith 
+ * @copyright Marvelpress Ltd. 2012
+ */
+
+$url            = "https://orders.marvelpress.com/products/";
+$APIUsername    = "MP-00213/testing";                   
+$APIPassword    = "b21efa4cdacfd551e93e4b76ee7c3667";
+
+//Initialise cURL
+$ch = curl_init();
+
+//Set the url and AUTH. cURL uses GET by default
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch,CURLOPT_URL,$url);
+curl_setopt($ch, CURLOPT_USERPWD, $APIUsername.':'.$APIPassword);
+
+//Execute post and decode response
+$result = json_decode(curl_exec($ch));
+
+//Get http status
+$http_status = curl_getinfo($ch , CURLINFO_HTTP_CODE);
+
+curl_close($ch);
+
+//Check if the request was successful 
+if($http_status == 200)
+{
+    // Result now contains associative array of products
+    print_r($result);
+}
+else
+{
+    echo "Sending Order Failed \n";
+    echo $result->Message."\n";
+}
